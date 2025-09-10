f7ded3e443cc937c297973143f0583ed51a945ab
jorpanp <johannesp02@gmail.com>
2025-09-10 11:36:25 +0200

    Added Flask app with requirements and gitignore

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Bro!"
+def hello_world():
+    return '<h1>Hello, World!</h1>'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/about")
+def about():
+    return '<h1>Om-sida</h1><p>Detta är en enkel Flask-app deployad på Render.</p>'
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
